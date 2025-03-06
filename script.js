fd5703aa48e2884a2318988aa855b4afd5b83df8
Manisha2505 <manishachandra.25@gmail.com>
2025-03-06 16:38:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -27,4 +27,14 @@ class Cat extends Animal {
 // Do not change the code below this line
 window.Animal = Animal;
 window.Dog = Dog;
-window.Cat = Cat;
+
+
+// // Example usage:
+// const myCat = new Cat("Siamese");
+// myCat.makeSound(); // Expected Output: The Siamese makes a sound
+// myCat.purr(); // Expected Output: purr
+
+// const myDog = new Dog("Golden Retriever");
+// myDog.makeSound(); // Expected Output: The Golden Retriever makes a sound
+// myDog.bark(); // Expected Output: woof
+// window.Cat = Cat;
